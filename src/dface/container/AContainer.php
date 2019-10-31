dba06988b43ab53b5ca080de111d0aaec530294b
Denis Ponomarev <ponomarev@gmail.com>
2019-10-31 17:28:02 +0200

    AContainer getNames()

--- src/dface/container/AContainer.php
+++ src/dface/container/AContainer.php
@@ -9,6 +9,8 @@ class AContainer extends BaseContainer
 
 	/** @var ContainerInterface */
 	private $container;
+	/** @var string[] */
+	private $names;
 
 	public function __construct(array $definitions = [], ContainerInterface $parent = null)
 	{
@@ -16,6 +18,7 @@ class AContainer extends BaseContainer
 		$factories = new FactoryContainer($definitions, $lookup);
 		$singleton = new SingletonContainer($factories);
 		$this->container = new PathResolver($singleton);
+		$this->names = \array_keys($definitions);
 	}
 
 	public function get($id)
@@ -26,6 +29,11 @@ class AContainer extends BaseContainer
 	public function has($id) : bool
 	{
 		return $this->container->has($id);
+	}
+
+	public function getNames() : array
+	{
+		return $this->names;
 	}
 
 }
