6e2b94d6faa6a0b132f933bd1d8e0366492d2823
Denis Ponomarev <ponomarev@gmail.com>
2021-01-19 15:18:15 +0200

    accept directories as definitions too

--- src/dface/container/FileDefinitionSource.php
+++ src/dface/container/FileDefinitionSource.php
@@ -6,11 +6,14 @@ class FileDefinitionSource implements DefinitionSource
 {
 
 	private string $dir;
+	/** @var callable|null */
+	private $dir_loader;
 	private array $resolved = [];
 
-	public function __construct(string $dir)
+	public function __construct(string $dir, ?callable $dir_loader = null)
 	{
 		$this->dir = \realpath($dir);
+		$this->dir_loader = $dir_loader;
 	}
 
 	/**
@@ -31,41 +34,94 @@ class FileDefinitionSource implements DefinitionSource
 	 */
 	public function getDefinition(string $name) : callable
 	{
-		$filename = $this->resolve($name);
-		if ($filename === null) {
+		$loader = $this->resolve($name);
+		if ($loader === null) {
 			throw new NotFoundException("'$name' not found in '$this->dir'");
 		}
-		/** @noinspection PhpIncludeInspection */
-		return include $filename;
+		return $loader();
 	}
 
 	public function getNames() : iterable
 	{
 		$glob_files = \glob($this->dir.'/*.php');
-		foreach ($glob_files as $filename) {
-			yield \substr(\basename($filename), 0, -4);
+		$file_defs = [];
+		foreach ($glob_files as $file_name) {
+			$def_name = \substr(\basename($file_name), 0, -4);
+			$file_defs[$def_name] = true;
+			yield $def_name;
+		}
+		if($this->dir_loader){
+			$glob_dirs = \glob($this->dir.'/*', GLOB_ONLYDIR);
+			foreach ($glob_dirs as $dir_name){
+				$def_name = \basename($dir_name);
+				if(!isset($file_defs[$def_name])){
+					yield $def_name;
+				}
+			}
 		}
 	}
 
 	/**
-	 * @param $id
-	 * @return string|null
+	 * @param $name
+	 * @return callable|null
 	 * @throws ContainerException
 	 */
-	private function resolve($id) : ?string
+	private function resolve($name) : ?callable
 	{
-		if (!\array_key_exists($id, $this->resolved)) {
-			$file_name = null;
-			$check_name = $this->dir.'/'.$id.'.php';
-			if (\file_exists($check_name)) {
-				$file_name = \realpath($check_name);
-				if(\strpos($file_name, $this->dir) !== 0){
-					throw new ContainerException("'$id' is out of '$this->dir'");
-				}
+		if (!\array_key_exists($name, $this->resolved)) {
+			$result = $this->checkFile($name);
+			if(!$result && $this->dir_loader){
+				$result = $this->checkDir($name);
 			}
-			$this->resolved[$id] = $file_name;
+			$this->resolved[$name] = $result;
+			return $result;
 		}
-		return $this->resolved[$id];
+		return $this->resolved[$name];
+	}
+
+	/**
+	 * @param $name
+	 * @return \Closure|null
+	 * @throws ContainerException
+	 */
+	private function checkFile($name) : ?callable
+	{
+		$this->preventNamingAbusing($name);
+		$file_name = $this->dir.'/'.$name.'.php';
+		if (\is_file($file_name)) {
+			return static function() use ($file_name){
+				/** @noinspection PhpIncludeInspection */
+				return include $file_name;
+			};
+		}
+		return null;
+	}
+
+	/**
+	 * @param $name
+	 * @return \Closure|null
+	 * @throws ContainerException
+	 */
+	private function checkDir($name) : ?callable
+	{
+		$this->preventNamingAbusing($name);
+		$dir_name = $this->dir.'/'.$name;
+		if (\is_dir($dir_name)) {
+			return function() use ($dir_name){
+				return ($this->dir_loader)($dir_name);
+			};
+		}
+		return null;
+	}
+
+	/**
+	 * @param string $name
+	 * @throws ContainerException
+	 */
+	private function preventNamingAbusing(string $name) : void {
+		if(\basename($name) !== $name){
+			throw new ContainerException("'$name' violates naming restriction");
+		}
 	}
 
 }
